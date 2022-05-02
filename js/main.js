d21a3b7e49ef676d1449335057fcf89053940932
EttoreBarcella <ettore.barcella@gmail.com>
2022-05-02 14:17:43 +0200

    added / 3 and / 5

--- js/main.js
+++ js/main.js
@@ -9,5 +9,22 @@
 const list= document.querySelector(".list");
 
 for( let i = 1; i <= 100; i++ ){
-    console.log(i);
+    const element = document.createElement("li");
+
+    element.classList.add("list-item");
+    element.innerText = i;
+    list.append(element)
+
+    if(i % 3 === 0){
+        element.classList.add("bg-three")
+        element.innerText = "Fizz";
+        list.append(element)
+    }else if(i % 5 === 0){
+        element.classList.add("bg-five")
+        element.innerText = "Buzz";
+        list.append(element)
 }
+
+
+
+
